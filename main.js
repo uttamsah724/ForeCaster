211417237590b8b396906e0f3d6cc7b4d1aac479
Uttam Sah <uttamkumarsah111@gmail.com>
2024-03-17 19:43:34 +0530

    simplify

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -19,32 +18,9 @@ async function checkWeather(city){
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-  
-    if(data.weather[0].main == "Clouds")
-    {
-      weatherIcon.src = src = "images/clouds.png";
-    }
-    else if(data.weather[0].main == "Clear"){
-      weatherIcon.src = src = "images/clear.png";
-    }
-    else if(data.weather[0].main == "Rain"){
-      weatherIcon.src = src = "images/rain.png";
-    }
-    else if(data.weather[0].main == "Drizzle"){
-      weatherIcon.src = src = "images/drizzle.png";
-    }
-    else if(data.weather[0].main == "Mist"){
-      weatherIcon.src = src = "images/mist.png";
-    }
-    else if(data.weather[0].main == "Snow"){
-      weatherIcon.src = src = "images/snow.png";
-    }
-    else{
-      weatherIcon.src = src = "images/wind.png";
-    }
 
     document.querySelector(".weather").style.display = "block"; 
-    document.querySelector(".error").style.display = "none"; 
+    document.querySelector(".error").style.display = "none"; //doing this because in the error handling it changed to "block" but now it should change
   }
 }
 
